1f299ce99777cf8a840dfa8f1af75406f4cbdca8
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2024-03-15 22:02:13 +0530

    Time: 14 ms (74.02%), Space: 27 MB (45.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,16 +6,16 @@ public:
         for(int i=1;i<nums.size();i++)
         {
             prefix[i]=prefix[i-1]*nums[i];
-            cout<<prefix[i]<<endl;
+            // cout<<prefix[i]<<endl;
         }
         int back=1;
         vector<int> ans(nums.size(),0);
         for(int i=nums.size()-1;i>0;i--)
         {   
-            cout<<prefix[i-1]<<" "<<back<<endl;
+            // cout<<prefix[i-1]<<" "<<back<<endl;
             ans[i]=prefix[i-1]*back;
             back*=nums[i];
-            cout<<ans[i]<<endl;
+            // cout<<ans[i]<<endl;
         }
         ans[0]=back;
         return ans;
