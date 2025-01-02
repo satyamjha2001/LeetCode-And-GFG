07e52ea547f0966fd5f0cbe2617d36a4fdb176a9
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2025-01-02 17:26:54 +0530

    Time: 0 ms (100%), Space: 11.1 MB (42.17%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
+    int lowerBound(vector<int>& nums, int x) {
+        int n = nums.size();
+        int index = n;
+        int start = 0, end = n - 1, mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] >= x) {
+                index = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        //sort the array
+        // sort the array
         sort(nums.begin(), nums.end());
-
-        for(int x=0;x<=n;x++)
-        {   //now we implement binary search and find first >= element index
-            int index=n,count=0;   
-            int start=0, end=n-1,mid;
-            while(start<=end)
-            {
-                mid=start+(end-start)/2;
-                if(nums[mid]>=x)
-                {
-                    index=mid;
-                    end=mid-1;
-                }
-                else
-                {
-                    start=mid+1;
-                }
+        int start = 0, end = n, mid;
+        while (start <= end) { 
+            mid = start + (end - start) / 2;
+            // now we implement binary search and find first >= element index
+            int index = lowerBound(nums, mid);
+            int count = n - index;
+            if (mid == count)
+                return mid;
+            else if (mid < count) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            count = n-index;
-            if(x==count)
-                return x;
         }
         return -1;
     }
