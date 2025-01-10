a4bd403efa00bfc699253813abaab0d325da355a
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2025-01-10 16:22:16 +0530

    Time: 1134 ms (5.02%), Space: 434.9 MB (5.02%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.cpp
+++ 0916-word-subsets/0916-word-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isSubset(vector<int> temp, vector<int> frequency2) {
+    bool isSubset(map<char, int> temp, map<char, int> frequency2) {
         for (int i = 0; i < 26; i++) {
             if (temp[i]<frequency2[i]) {
                 return false;
@@ -10,20 +10,19 @@ public:
     }
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> ans;
-        vector<int> frequency2(26, 0); // for storing maximum freq of words2
+        map<char,int> frequency2; // for storing maximum freq of words2
 
         // Step 1: Build the `frequency2` array
         for (string& word : words2) {
-            vector<int> temp(26, 0); // for storing frequency of char of each word of words2
+            map<char,int> temp; // for storing frequency of char of each word of words2
             for (char& ch : word) {
                 temp[ch - 'a']++;
-                frequency2[ch - 'a'] =
-                    max(frequency2[ch - 'a'], temp[ch - 'a']);
+                frequency2[ch - 'a'] = max(frequency2[ch - 'a'], temp[ch - 'a']);
             }
         }
         // Step 2: Check each word in words1 against the `frequency2` array
         for (string& word : words1) {
-            vector<int> temp(26, 0); // Frequency array for the current word in words1
+            map<char,int> temp; // Frequency array for the current word in words1
             for (char& ch : word) {
                 temp[ch - 'a']++;
             }
