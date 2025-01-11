1a4c672b56cf520a8e95091255d0b95359be65d9
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2025-01-11 22:51:59 +0530

    Time: 2 ms (75.52%), Space: 14.8 MB (66.78%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.length()<k)
+            return false;
+        int oddFreqCount=0;
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]%2==1)
+                oddFreqCount++;
+        }
+
+        return oddFreqCount<=k;
+    }
+};
