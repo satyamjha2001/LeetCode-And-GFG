287c12abcf884bedc24c145b3c134434266f8a86
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2025-01-12 23:30:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -2,40 +2,34 @@ class Solution {
 public:
     bool canBeValid(string s, string locked) {
         int n = s.length();
-        // if the string is odd than parantheses not matched
         if (n % 2 == 1)
             return false;
-        stack<int> open;
-        stack<int> openClose;
+        // approach 2
+        // in this we don't use stack and extra space
+        int openBracketCount = 0;
+        int closedBracketCount = 0;
+        // in this we count open bracket from L to R so we use greedy so where
+        // locked =0 we assume open bracket.
         for (int i = 0; i < n; i++) {
-            if (locked[i] == '0') {
-                openClose.push(i); // we need to push index not character and we
-                                   // store all changeble parentheses.
-            } else if (s[i] == '(') {
-                open.push(i); // we push open parentheses
-            } else if (s[i] == ')') {
-                if (!open.empty()) {
-                    open.pop(); // if open stack have parentheses than we pop
-                                // first from here.
-                } else if (!openClose.empty()) {
-                    openClose.pop(); // if open empty then we pop from changeble
-                                     // stack
-                } else {
-                    return false; // if both stack is empty than we return
-                                  // false.
-                }
-            }
+            if (s[i] == '(' || locked[i] == '0')
+                openBracketCount++;
+            else if (s[i] == ')')
+                openBracketCount--;
+            if (openBracketCount < 0)  //if -ve we immediately return false because we can't balance.
+                return false;
         }
 
-        // now one case is that where both stack are non empty. so we try to
-        // balance from both the stack.
-        while (!open.empty() && !openClose.empty() &&
-               open.top() < openClose.top()) {
-            open.pop();
-            openClose.pop();
+        // in this we count close bracket from R to L so we use greedy so where
+        // locked = 0 we assume close bracket.
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ')' || locked[i] == '0')
+                closedBracketCount++;
+            else
+                closedBracketCount--;
+            if (closedBracketCount < 0) 
+                return false;
         }
-        // now if open is empty we return true because even no ch are present in
-        // openClose stack. if open is not empty than it return false.
-        return open.empty();
+
+        return true;
     }
 };
