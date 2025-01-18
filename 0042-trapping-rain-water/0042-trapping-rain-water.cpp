c1ab639a5f3459c4b6bfaabed7ced2ead1f0ff46
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2025-01-18 19:06:32 +0530

    Time: 0 ms (100%), Space: 26 MB (54.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size(), water=0;
-        vector<int> leftMax(n,0);
-        vector<int> rightMax(n,0);
-        for(int i=1;i<n;i++)
-        {
-            leftMax[i]=max(leftMax[i-1], height[i-1]);
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            rightMax[i]=max(rightMax[i+1], height[i+1]);
-        }
+        int n=height.size(), water=0,maxHeight=0,index=-1,leftMax=0,rightMax=0;
 
+        //find maximum height
         for(int i=0;i<n;i++)
         {
-            if((min(rightMax[i], leftMax[i])-height[i])>0)
-                water+=(min(rightMax[i], leftMax[i])-height[i]);
+            if(maxHeight<height[i])
+            {
+                index=i;
+                maxHeight=height[i];
+            }
+        }
+
+        //now traverse in left part of max height
+        for(int i=1;i<index;i++) //0th index not store water
+        {
+            leftMax=max(leftMax,height[i-1]);
+            if(leftMax-height[i]>0)
+            {
+                water+=leftMax-height[i];
+            }
+        }
+
+        //now traverse in right part of max height
+        for(int i=n-2;i>index;i--) //n-1 index not store water
+        {
+            rightMax=max(rightMax,height[i+1]);
+            if(rightMax-height[i]>0)
+            {
+                water+=rightMax-height[i];
+            }
         }
         return water;
     }
